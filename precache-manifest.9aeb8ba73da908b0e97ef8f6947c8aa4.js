1523130998d1fd64ed9c57f44497d84b8bc20576
Joe Marino <joeycmarino@gmail.com>
2019-11-18 13:58:09 -0500

    Updates

---
+++ precache-manifest.9aeb8ba73da908b0e97ef8f6947c8aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669a75deb7832d80015319645dd47e96",
+    "url": "/matching-game/index.html"
+  },
+  {
+    "revision": "3aadb60c398c5d291862",
+    "url": "/matching-game/static/css/main.69b3ab0e.chunk.css"
+  },
+  {
+    "revision": "0426b9d61d2c25821cf1",
+    "url": "/matching-game/static/js/2.02c30aef.chunk.js"
+  },
+  {
+    "revision": "3aadb60c398c5d291862",
+    "url": "/matching-game/static/js/main.30fc526a.chunk.js"
+  },
+  {
+    "revision": "ea20f7c1839ff9273084",
+    "url": "/matching-game/static/js/runtime-main.fb02cceb.js"
+  },
+  {
+    "revision": "6f465c9376f8fff1cc069589884f46dc",
+    "url": "/matching-game/static/media/CardBack.6f465c93.png"
+  }
+]);
